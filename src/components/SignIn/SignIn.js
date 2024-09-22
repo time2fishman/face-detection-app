25b6ff5e9ff09780e9be7a021167b76726eeed4f
time2fishman <adam_h_martinez@hotmail.com>
2024-09-22 16:54:52 -0600

    fix register button on sign in page

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -61,7 +61,7 @@ const SignIn = ({ onRouteChange, loadUser }) => {
             <input onClick={onSubmitSignIn} className="b br3 ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
           </div>
           <div className="lh-copy mt3">
-            <p onClick={onSubmitSignIn} className="fw5 f6 link dim black db pointer">Register</p>
+            <p onClick={onRouteChange} className="fw5 f6 link dim black db pointer">Register</p>
           </div>
         </div>
       </main>
